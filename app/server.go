ca265aa52750fcaa644a4c029b1b83fcc3cea9ff
Rafael Toledo <toledorafael304@gmail.com>
2024-06-16 14:37:10 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -15,19 +15,41 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		handleConnections(conn)
 	}
+}
 
-	req := make([]byte, 1024)
-	conn.Read(req)
+func handleConnections(conn net.Conn) {
+	defer conn.Close()
 
-	if strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+	requestBuffer := make([]byte, 1024)
+
+	n, err := conn.Read(requestBuffer)
+	if err != nil {
+		fmt.Println("Failed to read request: ", err)
+		return
+	}
+	fmt.Printf("Request: %s\n", requestBuffer[:n])
+
+	request := string(requestBuffer[:n])
+	path := strings.Split(request, " ")[1]
+
+	if path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.Split(path, "/")[1] == "echo" {
+		message := strings.Split(path, "/")[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
+
 }
