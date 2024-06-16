938210ce3dcce2c096843b3768fcccc315f79afe
Rafael Toledo <toledorafael304@gmail.com>
2024-06-16 16:07:37 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -41,28 +41,43 @@ func handleConnections(conn net.Conn) {
 	fmt.Printf("Request: %s\n", requestBuffer[:n])
 
 	request := string(requestBuffer[:n])
+	method := strings.Split(request, " ")[0]
 	path := strings.Split(request, " ")[1]
 
-	if path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.Split(path, "/")[1] == "echo" {
-		message := strings.Split(path, "/")[2]
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
-	} else if strings.Split(path, "/")[1] == "user-agent" {
-		userAgent := strings.TrimSpace(strings.Split(request, "User-Agent:")[1])
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
-	} else if strings.Split(path, "/")[1] == "files" {
-		requestedFile := strings.Split(path, "/")[2]
-		dir := os.Args[2]
-		content, err := os.ReadFile(dir + requestedFile)
-		if err != nil {
+	if method == "GET" {
+		if path == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else if strings.Split(path, "/")[1] == "echo" {
+			message := strings.Split(path, "/")[2]
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+		} else if strings.Split(path, "/")[1] == "user-agent" {
+			userAgent := strings.TrimSpace(strings.Split(request, "User-Agent:")[1])
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
+		} else if strings.Split(path, "/")[1] == "files" {
+			requestedFile := strings.Split(path, "/")[2]
+			dir := os.Args[2]
+			content, err := os.ReadFile(dir + requestedFile)
+			if err != nil {
+				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				return
+			}
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(string(content)), string(content))))
+		} else {
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			return
 		}
+	} else if method == "POST" {
+		if strings.Split(path, "/")[1] == "files" {
+			requestedFile := strings.Split(path, "/")[2]
+			dir := os.Args[2]
 
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(string(content)), string(content))))
+			lines := strings.Split(request, "\n")
+			content := lines[len(lines)-1]
+
+			os.WriteFile(dir+requestedFile, []byte(content), 0644)
+
+			conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
+		}
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-
 }
